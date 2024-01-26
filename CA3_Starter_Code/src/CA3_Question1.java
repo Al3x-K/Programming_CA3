93a989b486729e7827c8b354f5dd1dddb6a73000
Al3x-K <ola.kail1901@gmail.com>
2024-01-26 15:37:08 +0000

    added sample code

---
+++ CA3_Starter_Code/src/CA3_Question1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ *  Name: Aleksandra Kail
+ *  Class Group: GD2B
+ */
+public class CA3_Question1
+{
+    public static void runSimulation()
+    {
+        Scanner keyboard = new Scanner(System.in);
+        Stack<Integer> driveway = new Stack<Integer>(); //Creating a stack for parked cars
+        Stack<Integer> street = new Stack<Integer>(); //Creating a stack for cars retrieved
+        int num;
+        int currentNum;
+        do
+        {
+            System.out.println("Positive number -> park the car in the driveway");
+            System.out.println("Negative number -> retrieve the car");
+            System.out.println("0 -> Exit the application");
+            System.out.print("Choose the option: ");
+            num = keyboard.nextInt();
+            currentNum = num;
+            if(num > 0)
+            {
+                driveway.push(num);
+                System.out.println(driveway);
+            }
+            else if(num < 0)
+            {
+                
+            }
+        }
+        while(num != 0);
+    }
+
+    public static void main(String[] args) {
+        runSimulation();
+    }
+}
