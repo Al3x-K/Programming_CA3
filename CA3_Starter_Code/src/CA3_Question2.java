136cbca6cdcba9d644bf6e7ffba36c2e5295dff9
Al3x-K <ola.kail1901@gmail.com>
2024-01-31 23:31:58 +0000

    starting question 2 again from scratch

--- CA3_Starter_Code/src/CA3_Question2.java
+++ CA3_Starter_Code/src/CA3_Question2.java
@@ -2,86 +2,52 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- *  Name: Aleksandra Kail
- *  Class Group: GD2B
+ *  Name:
+ *  Class Group:
  */
-public class CA3_Question2 {
+public class CA3_Question2
+{
     /*
         Starter function to create the 2D array and populate it with 0
 
      */
-    public static int[][] floodFillStart() {
+    public static int[][]  floodFillStart() {
         Scanner kb = new Scanner(System.in);
         int[][] arr = new int[10][10];
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
                 arr[x][y] = 0;
             }
         }
         return arr;
     }
-
     /*
         Helper function to display the image
      */
-    public static void display(int[][] arr) {
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
+    public static void display(int[][] arr)
+    {
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
                 System.out.printf("%4d", arr[x][y]);
             }
             System.out.println();
         }
     }
-
     private static void fill(int r, int c, int[][] arr)
     {
-        Stack<Pair> cell = new Stack<Pair>();
-        int count = 1;
-        Pair current = new Pair(3,7);
-        //current = cell.pop();
-        int x = current.getX();
-        int y = current.getY();
-        arr[x][y] = count;
-        while (!cell.isEmpty())
-        {
 
-            //NORTH
-            if(x > 0 && arr[x-1][y] == 0)
-            {
-                arr[x-1][y] = count;
-                cell.push(new Pair(x-1,y));
-            }
-            //EAST
-            if(y<9 && arr[x][y+1] == 0)
-            {
-                arr[x][y+1] = count;
-                cell.push(new Pair(x,y+1));
-            }
-            //SOUTH
-            if(x > 0 && arr[x+1][y] == 0)
-            {
-                arr[x+1][y] = count;
-                cell.push(new Pair(x+1,y));
-            }
-            //WEST
-            if(y>0 && arr[x][y-1] == 0)
-            {
-                arr[x][y-1] = count;
-                cell.push(new Pair(x,y-1));
-            }
-            count++;
-        }
     }
 
-    public static void start() {
+    public static void start()
+    {
         int[][] arr = floodFillStart();
-        display(arr);
-        fill(10,10,arr);
-        display(arr);
     }
-
     public static void main(String[] args) {
         start();
     }
-}
 
+}
