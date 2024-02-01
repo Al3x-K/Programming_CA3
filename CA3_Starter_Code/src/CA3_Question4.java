60ead7bfec3601f5ba7c4b1838c2f6be988903a0
Al3x-K <ola.kail1901@gmail.com>
2024-02-01 03:25:41 +0000

    version 1 of question 4
    the program works. but after executing it for the 3rd time something broke and it cannot find the files anymore

--- CA3_Starter_Code/src/CA3_Question4.java
+++ CA3_Starter_Code/src/CA3_Question4.java
@@ -3,8 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
 /**
- *  Name:
- *  Class Group:
+ *  Name:Aleksandra Kail
+ *  Class Group: GD2B
  */
 public class CA3_Question4 {
 
@@ -13,7 +13,33 @@ public class CA3_Question4 {
      */
     public static boolean validate(String filename) throws FileNotFoundException
     {
-        return false;
+        Scanner scanner = new Scanner(new File(filename)); //Read tags from the file
+        String [] tags = scanner.nextLine().split(" "); //puts tags into the array and split them by space
+
+        Stack<String> stack = new Stack<>(); //a stack for opening tags
+
+        for(String tag : tags) //goes through each tag in the array
+        {
+            if(tag.startsWith("</")) //checks if it's a closing tag
+            {
+                if(stack.isEmpty()) //there's a closing tag without the opening one
+                {
+                    return false;
+                }
+
+                String opTag = stack.pop();
+                //checks if the closing tag matches the tag at the top of the stack
+                if(!tag.substring(2, tag.length() - 1).equals(opTag.substring(1)))
+                {
+                    return false;
+                }
+                else
+                {
+                    stack.push(tag); //if it's an opening tag it gets pushed onto the stack
+                }
+            }
+        }
+        return stack.isEmpty(); //if the stack is empty at the end, that means that all the tags are properly nested
     }
 
     /*
@@ -24,13 +50,18 @@ public class CA3_Question4 {
 
 
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException
+    {
         String[] files = {"tags_valid.txt", "tags_invalid.txt"};
-        for(String fName: files) {
+        for(String fName: files)
+        {
             System.out.print(fName +": ");
-            if (validate(fName)) {
+            if (validate(fName))
+            {
                 System.out.println("This file is valid");
-            } else {
+            }
+            else
+            {
                 System.out.println("This file is invalid");
             }
         }
